7e80278c60337422188e2d8b6728acdc85c561c0
jemerardprete <julie.emerardprete1@ipilyon.net>
2020-12-02 16:39:10 +0100

    Ajout fonctionnalités dans templates et controllers

--- src/main/java/com/myaudiolibrary/web/controller/ArtistController.java
+++ src/main/java/com/myaudiolibrary/web/controller/ArtistController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
 import javax.persistence.EntityNotFoundException;
 import java.util.Optional;
@@ -40,6 +42,15 @@ public class ArtistController {
         model.put("artist", artist);
         return "detailArtist";
     }
+    /*@RequestMapping(value = "", method = RequestMethod.GET, produces = "application/json", params = {"name", "page", "size", "sortProperty", "sortDirection"})
+    public Page<Artist> searchByName(@RequestParam (value = "name") String name,
+                                     @RequestParam (value = "page", defaultValue = "0") Integer page,
+                                     @RequestParam (value = "size", defaultValue = "10") Integer size,
+                                     @RequestParam (value = "sortProperty") String sortProperty,
+                                     @RequestParam (value = "sortDirection") String sortDirection){
+        PageRequest pageRequest = PageRequest.of(page, size, Sort.Direction.fromString(sortDirection), sortProperty);
+        return artistRepository.findByNameContaining(name, pageRequest);
+    }*/
 
     // Liste des artistes
     @RequestMapping(value = "", method = RequestMethod.GET)
@@ -50,13 +61,37 @@ public class ArtistController {
                                @RequestParam(defaultValue = "name") String sortProperty) {
         PageRequest pageRequest = PageRequest.of(page, size, Sort.Direction.fromString(sortDirection), sortProperty);
         Page<Artist> pageArtist = artistRepository.findAll(pageRequest);
-        model.put("artistes", pageArtist);
+        model.put("artists", pageArtist);
         model.put("pageNumber", page + 1);
         model.put("previousPage", page - 1);
         model.put("nextPage", page + 1);
         model.put("start", page * size + 1);
         model.put ("end", (page) * size + pageArtist.getNumberOfElements());
         return "listeArtists";
+    }
+
+    // REVOIR CREATION, MODIFICATION, SUPPRESSION + GESTION ERREUR
+
+    // Création et modification d'un artiste
+    @RequestMapping(method = RequestMethod.POST, value = "", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public RedirectView createOrSaveArtist(Artist artist){
+        return saveArtist(artist);
+    }
+
+    // Redirection vers l'artiste venant d'être crée
+    private RedirectView saveArtist(Artist artist){
+        artist = artistRepository.save(artist);
+        return new RedirectView("/artists/" + artist.getId());
+    }
+
+    // Suppression => Rediriger vers la liste des artistes
+    @RequestMapping(method = RequestMethod.GET, value = "/{id}/delete")
+    public RedirectView deleteArtist(@PathVariable Integer id){
+        if(!artistRepository.existsById(id)){
+            throw new EntityNotFoundException("L'artiste d'identifiant " + id + " n'a pas été trouvé !");
+        }
+        artistRepository.deleteById(id);
+        return new RedirectView("/artists?page=0&size=10&sortProperty=name&sortDirection=ASC");
     }
 
 }
